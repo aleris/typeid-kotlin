cc6543f1bf6462a8be313cbf3aeccfd2677cffdc
Adrian Tosca <atosca@adobe.com>
2024-05-17 07:36:13 +0300

    add additional json serialization test for ids only

--- src/test/kotlin/earth/adi/typeid/JacksonJsonTest.kt
+++ src/test/kotlin/earth/adi/typeid/JacksonJsonTest.kt
@@ -1,5 +1,6 @@
 package earth.adi.typeid
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.BeanProperty
 import com.fasterxml.jackson.databind.PropertyName
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -8,7 +9,7 @@ import earth.adi.typeid.jackson.IdJsonDeserializer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class JsonTest {
+class JacksonJsonTest {
   private val defaultObjectMapper = jacksonObjectMapper().registerModule(TypeId.jacksonModule())
 
   @Test
@@ -52,27 +53,28 @@ class JsonTest {
     assertThat(writtenJson).isEqualTo(JSON_USER_AND_ORGANIZATION)
   }
 
-  companion object {
-    private val JSON_USER =
-        """
-      {
-        "id" : "user_01hy0d96sgfx0rh975kqkspchh"
-      }
-      """
-            .trimIndent()
+  data class IdsOnly(
+      @JsonProperty("user_id") val userId: UserId,
+      @JsonProperty("org_id") val organizationId: OrganizationId,
+  )
 
-    private val JSON_USER_AND_ORGANIZATION =
-        """
-      {
-        "user" : {
-          "id" : "user_01hy0d96sgfx0rh975kqkspchh"
-        },
-        "organization" : {
-          "id" : "org_01hy0sk45qfmdsdme1j703yjet"
-        }
-      }
-      """
-            .trimIndent()
+  @Test
+  fun `deserialize ids only from json`() {
+    val json = objectMapper.readValue<IdsOnly>(JSON_IDS_ONLY)
+    assertThat(json.userId).isEqualTo(typeId.parse<User>("user_01hy0d96sgfx0rh975kqkspchh"))
+    assertThat(json.organizationId)
+        .isEqualTo(typeId.parse<Organization>("org_01hy0sk45qfmdsdme1j703yjet"))
+  }
+
+  @Test
+  fun `serialize ids only to json`() {
+    val json =
+        IdsOnly(
+            typeId.parse<User>("user_01hy0d96sgfx0rh975kqkspchh"),
+            typeId.parse<Organization>("org_01hy0sk45qfmdsdme1j703yjet"),
+        )
+    val writtenJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json)
+    assertThat(writtenJson).isEqualTo(JSON_IDS_ONLY)
   }
 
   @Test
@@ -97,5 +99,37 @@ class JsonTest {
     val idJsonDeserializer =
         IdJsonDeserializer(typeId()).createContextual(null, null) as IdJsonDeserializer
     assertThat(idJsonDeserializer.valueType).isNull()
+  }
+
+  companion object {
+    private val JSON_USER =
+        """
+      {
+        "id" : "user_01hy0d96sgfx0rh975kqkspchh"
+      }
+      """
+            .trimIndent()
+
+    private val JSON_USER_AND_ORGANIZATION =
+        """
+      {
+        "user" : {
+          "id" : "user_01hy0d96sgfx0rh975kqkspchh"
+        },
+        "organization" : {
+          "id" : "org_01hy0sk45qfmdsdme1j703yjet"
+        }
+      }
+      """
+            .trimIndent()
+
+    private val JSON_IDS_ONLY =
+        """
+      {
+        "user_id" : "user_01hy0d96sgfx0rh975kqkspchh",
+        "org_id" : "org_01hy0sk45qfmdsdme1j703yjet"
+      }
+      """
+            .trimIndent()
   }
 }
